0fdc78044fb6cb37222589d2e2def5c00cb6ee04
Sungwon Jo <doriya@nexell.co.kr>
2016-02-18 16:22:41 +0900

    [Android|Application] * Add Default Application. ( NxVideoPlayer, NxAudioPlayer )

--- device.mk
+++ device.mk
@@ -344,5 +344,6 @@ $(call inherit-product-if-exists, hardware/samsung_slsi/slsiap/slsiap.mk)
 #$(call inherit-product-if-exists, vendor/google/gapps/gapps.mk)
 
 # Nexell Application
-#$(call inherit-product-if-exists, vendor/nexell/apps/nxapps.mk)
+$(call inherit-product-if-exists, vendor/nexell/apps/nxvideoplayer.mk)
+$(call inherit-product-if-exists, vendor/nexell/apps/nxaudioplayer.mk)
 $(call inherit-product-if-exists, vendor/nexell/apps/smartsync.mk)
